aa1521094cc874c00cd4bf6ec40408fb832240a4
Zachariah Jeter <Zachjeter95@icloud.com>
2023-03-21 12:32:06 -0700

    Created a handle submit to successfully send request to the backend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar'
+import axios from 'axios';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: [],
+      listBuisineses: [],
+
+    }
+  }
+
+  handleInput = (e) => {
+    this.setState({
+      userInput: [e.target[0].value, e.target[1].value]
+
+    })
+  };
+
+  getBusinessData = async (search, location) => {
+    try {
+
+      let reqToServer = await axios.get(`${process.env.REACT_APP_SERVER}/company?search=${search}&location=${location}`);
+
+      this.setState({
+        listBuisineses: reqToServer.data,
+      });
+
+    } catch (error) {
+
+      this.setState({
+        error: true,
+        errorMsg: `ERROR: ${error.response.status}`
+      })
+    }
+  }
+
+
+
+
   render() {
     return (
-      <Navbar/>
-  );
-}
+      <Navbar
+        handleInput={this.handleInput}
+      />
+    );
+  }
 }
 
 export default App;
